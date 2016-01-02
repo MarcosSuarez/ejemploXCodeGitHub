5ecc83e5b3f9ece171e7d4ba097567dbf115e678
Marcos <marcos.suarez@rocketmail.com>
2016-01-02 22:42:20 +0100

    Cambio comentarios ViewController

--- pruebaGit/ViewController.swift
+++ pruebaGit/ViewController.swift
@@ -12,12 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Despues de Cargar Visat, ideal para nib
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Memoria a eliminar en caso de requerirlo.
     }
 
 
